05d3482c470a89367c4d4d5dc931ecdf6b29e1be
Drfox81 <Drfox11181@mail.ru>
2023-01-21 03:04:58 +0300

    Проверил, роаботает!)

--- src/main/java/com/example/demo/services/RecipeService.java
+++ src/main/java/com/example/demo/services/RecipeService.java
@@ -1,10 +1,16 @@
 package com.example.demo.services;
 
 import com.example.demo.model.Recipe;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface RecipeService {
 
     void addRecipe(Recipe recipe);
     Recipe getRecipe(String recipe);
 
+    Recipe editRecipe(String recipeName, Recipe recipeNew);
+
+    String getAllRecipe();
+
+    boolean deleteRecipe(@PathVariable String recipeName);
 }
